3c5f3e2983374d101968e3c69f7db854f0a1f7ce
Rutvik599 <126308040+Rutvik599@users.noreply.github.com>
2024-02-14 22:54:21 +0530

    Project Initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Button } from '@radix-ui/themes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='maindiv'>
+
+     <h1 className="font-bold  text-red-300 px-5" id='h1'>
+      Hello world!
+    </h1>
+    <Button variant="outline" className='text-zinc-500'>Button</Button>
     </div>
+    </>
   );
 }
 
